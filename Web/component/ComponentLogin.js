14303ca54f66640785626bdc9bcd9cbec43f8716
joshdeng <j.deng7@hotmail.com>
2016-10-14 15:19:32 -0600

    login content done

---
+++ Web/component/ComponentLogin.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { Textfield,Grid,Cell,Card,CardText, CardActions, Button } from 'react-mdl';
+
+var componentStyle = {
+    margin: 'auto',
+}
+
+var formStyle = {
+    marginTop: '5%'
+}
+
+class ComponentLogin extends Component {
+
+    loginSubmit() {
+        var email = this.refs.email.value;
+        var pw    = this.refs.pw.value;
+        var self  = this;
+
+        firebase.auth().signInWithEmailAndPassword(email, pw).then(function(result) {
+            var location = self.props.location
+            if (location.state && location.state.nextPathname) {
+                self.context.router.replace(location.state.nextPathname)
+            } else {
+                self.context.router.replace('/home')
+            }
+            // User signed in!
+            console.log('User signed in!');
+            // var uid = result.user.uid;
+        });
+    }
+
+    render() {
+        return (
+                <form style={formStyle} onSubmit={this.loginSubmit}>
+                    <CardText style={componentStyle}>
+                        <Textfield className="form-control" ref="email" placeholder="Email" id="email"/>
+                    </CardText>
+                    <CardText style={componentStyle}>        
+                        <Textfield ref="pw" type="password" className="form-control" placeholder="Password" id="password"/>
+                    </CardText>
+                    {/*errors*/}
+                    <CardActions style={componentStyle}>
+                        <Button accent ripple type="submit" className="mdl-color-text--indigo btn btn-primary">Login</Button>
+                    </CardActions>
+                </form>
+        );
+    }
+
+};
+
+export default ComponentLogin
---
+++ Web/component/ContentLogin.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import {Content, Card,CardTitle} from 'react-mdl';
+import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router'
+import ComponentLogin from './ComponentLogin.js'
+
+var cardStyle = {
+    width: '80%',
+    margin: 'auto',
+    marginTop: '50px',
+    height:'500px'
+}
+
+var cardTitleStyle = {
+    center:'true'
+}
+
+class ContentLogin extends Component {
+    render(){
+        return(
+          <div  className="learn-content mdl-layout__content mdl-typography--text-center">
+              <Card shadow={0} style={cardStyle} >
+                <CardTitle className="mdl-color--indigo mdl-color-text--white mdl-shadow--2dp">Login</CardTitle>
+                <ComponentLogin/>
+              </Card>
+          </div>
+        );
+    }
+};
+
+
+
+export default ContentLogin
