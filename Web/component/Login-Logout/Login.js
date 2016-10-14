d4dd324f3cc4c7875a5e1d360ea9281a4031ce3a
joshdeng <j.deng7@hotmail.com>
2016-10-14 15:19:28 -0600

    login content done

--- Web/component/Login-Logout/Login.js
+++ Web/component/Login-Logout/Login.js
@@ -4,21 +4,18 @@ import LearnHeader from '../Header.js'
 import LearnFooter from '../Footer.js'
 import ContentHome from '../ContentHome.js'
 import ComponentLogin from '../ComponentLogin.js'
+import ContentLogin from '../ContentLogin.js'
 // var firebase = require('firebase');
 // var config = require('../../../firebase.config.js');
 // firebase.initializeApp(config);
 
 class Login extends Component {
   render(){
-    return (
-      <div>    
-          <div className="learn-header mdl-layout__header">
-            <LearnHeader/>
-          </div>
-          <div className="">
-            <ComponentLogin/>   
-          </div>
-          <LearnFooter/>
+    return (  
+      <div className="learn-header mdl-layout__header">
+        <LearnHeader/>
+        <ContentLogin/>   
+        <LearnFooter/>
       </div>    
     );
   }
