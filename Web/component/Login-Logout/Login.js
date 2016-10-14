670fad60c71d2ca724651101fffc85afd7279f11
joshdeng <j.deng7@hotmail.com>
2016-10-13 23:29:51 -0600

    commented var error

--- Web/component/Login-Logout/Login.js
+++ Web/component/Login-Logout/Login.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { Textfield,Grid,Cell } from 'react-mdl';
+import Header from '../Header.js'
+import Footer from '../Footer.js'
+
 // var firebase = require('firebase');
 // var config = require('../../../firebase.config.js');
 // firebase.initializeApp(config);
@@ -9,7 +12,7 @@ class Login extends Component {
     return (
       <div>    
           <div className="learn-header mdl-layout__header">
-            <LoginHeader/>
+            <Header/>
           </div>
           <div className="">
             <LoginMidDiv/>   
@@ -20,37 +23,7 @@ class Login extends Component {
   }
 }
 
-class LoginHeader extends Component {
 
-    render(){
-        return(
-            
-        <div className="mdl-layout__header-row">
-          <span className="learn-title mdl-layout-title">
-            <img className="learn-logo-image" src="images/learn_logo.png" />
-          </span>
-          {/* Add spacer, to align navigation to the right in desktop */}
-          <div className="mdl-layout-spacer" />
-
-          {/* Navigation */}
-          <div className="learn-navigation-container">
-            <nav className="learn-navigation mdl-navigation">
-              <a className="mdl-navigation__link mdl-typography--text-uppercase" href>Courses</a>
-              <a className="mdl-navigation__link mdl-typography--text-uppercase" href>Pricing</a>
-              <a className="mdl-navigation__link mdl-typography--text-uppercase" href>How it works</a>
-            </nav>
-          </div>
-          <button className="learn-signIn-button mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent mdl-color--indigo mdl-color-text--white mdl-shadow--dp" >
-            Login
-          </button>
-          <button className="learn-signUp-button mdl-button mdl-js-button mdl-button--accent mdl-color-text--indigo ">
-            Login
-          </button>
-        </div>
-      
-    );};
-    
-};
 
 class LoginMidDiv extends Component {
 
@@ -82,7 +55,8 @@ class LoginMidDiv extends Component {
             
                 <label>Password</label>
                 <input ref="pw" type="password" className="form-control" placeholder="Password" id="password"/>
-                {errors}
+                {/* *todo* heres a variable called errors that is not defined. i have commented it*/}
+                {/*errors*/}
                 <button type="submit" className="btn btn-primary">Login</button>
             </form>
             </div>
@@ -92,36 +66,5 @@ class LoginMidDiv extends Component {
 };
 
 
-class Footer extends Component {
-  render() {
-    return (
 
-      <footer className="android-footer mdl-mega-footer">
-        <div className="mdl-mega-footer--top-section">
-          <div className="mdl-mega-footer--left-section">
-            <button className="mdl-mega-footer--social-btn" />
-            &nbsp;
-            <button className="mdl-mega-footer--social-btn" />
-            &nbsp;
-            <button className="mdl-mega-footer--social-btn" />
-          </div>
-          <div className="mdl-mega-footer--right-section">
-            <a className="mdl-typography--font-light" href="#top">
-              Back to Top
-              <i className="material-icons">expand_less</i>
-            </a>
-          </div>
-        </div>
-        <div className="mdl-mega-footer--middle-section">
-          <p className="mdl-typography--font-light">Satellite imagery: © 2014 Astrium, DigitalGlobe</p>
-          <p className="mdl-typography--font-light">Some features and devices may not be available in all areas</p>
-        </div>
-        <div className="mdl-mega-footer--bottom-section">
-          <a className="android-link mdl-typography--font-light" href>Blog</a>
-          <a className="android-link mdl-typography--font-light" href>Privacy Policy</a>
-        </div>
-      </footer>
-    );
-  }
-};
 export default Login
