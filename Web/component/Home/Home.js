ac7a75df03628d77cb7ba72568055235bc906e89
joshdeng <j.deng7@hotmail.com>
2016-10-14 14:42:33 -0600

    cut login into components

--- Web/component/Home/Home.js
+++ Web/component/Home/Home.js
@@ -19,9 +19,4 @@ class Home extends Component {
 
 
 
-
-
-
-
-
 export default Home
--- Web/component/Login-Logout/Login.js
+++ Web/component/Login-Logout/Login.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import { Textfield,Grid,Cell } from 'react-mdl';
 import LearnHeader from '../Header.js'
 import LearnFooter from '../Footer.js'
-
+import ContentHome from '../ContentHome.js'
+import ComponentLogin from '../ComponentLogin.js'
 // var firebase = require('firebase');
 // var config = require('../../../firebase.config.js');
 // firebase.initializeApp(config);
@@ -15,7 +16,7 @@ class Login extends Component {
             <LearnHeader/>
           </div>
           <div className="">
-            <LoginContent/>   
+            <ComponentLogin/>   
           </div>
           <LearnFooter/>
       </div>    
@@ -25,45 +26,7 @@ class Login extends Component {
 
 
 
-class LoginContent extends Component {
 
-    loginSubmit() {
-        var email = this.refs.email.value;
-        var pw    = this.refs.pw.value;
-        var self  = this;
-
-        firebase.auth().signInWithEmailAndPassword(email, pw).then(function(result) {
-            var location = self.props.location
-            if (location.state && location.state.nextPathname) {
-                self.context.router.replace(location.state.nextPathname)
-            } else {
-                self.context.router.replace('/home')
-            }
-            // User signed in!
-            console.log('User signed in!');
-            // var uid = result.user.uid;
-        });
-    }
-
-    render() {
-        return (
-            <div>
-            <h1> Login </h1>
-            <form onSubmit={this.loginSubmit}>
-                <label> Email </label>
-                <input className="form-control" ref="email" placeholder="Email" id="email"/>
-            
-                <label>Password</label>
-                <input ref="pw" type="password" className="form-control" placeholder="Password" id="password"/>
-                {/* *todo* heres a variable called errors that is not defined. i have commented it*/}
-                {/*errors*/}
-                <button type="submit" className="btn btn-primary">Login</button>
-            </form>
-            </div>
-        );
-    }
-
-};
 
 
 
