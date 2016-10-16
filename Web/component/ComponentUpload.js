331adab1848da9b02353c0aa5d78d7712f6c93e5
FadedWill <FadedWill@mingtuodeMacBook-Pro.local>
2016-10-15 20:15:01 -0600

    upload recovered

--- Web/component/ComponentUpload.js
+++ Web/component/ComponentUpload.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react'
 import LearnHeader from './Header.js'
 import { Textfield,Grid,Cell,Card,CardText, CardActions, Button } from 'react-mdl';
-
+import FirebaseTools from './Firebase.js'
+    
 class ComponentUpload extends Component {
+    
+    handleFileSelect(evt) {
+      evt.stopPropagation();
+      evt.preventDefault();
+      var file = evt.target.files[0];
+      var fileName = file.name;
+      var metadata = {
+        'contentType': file.type
+      };
+      FirebaseTools.vendorUpload({fileName,file,metadata});
+    }
+    /* kept for later usage
+    init() {
+      document.getElementById('file').addEventListener('change', this.handleFileSelect());
+      document.getElementById('file').disabled = false;
+      auth.onAuthStateChanged(function(user) {
+        if (user) {
+          console.log('Anonymous user signed-in.', user);
+          document.getElementById('file').disabled = false;
+        } else {
+          console.log('There was no anonymous session. Creating a new anonymous user.');
+          // Sign the user in anonymously since accessing Storage requires the user to be authorized.
+          auth.signInAnonymously();
+        }
+      });
+    }
+    */
 
     render(){
         return(
-    <div  className="learn-content mdl-layout__content mdl-typography--text-center">
-    <div className="logo-font learn-slogan">Upload Test</div>
-    <div className="learn-content mdl-layout__content mdl-typography--text-center">
-      <div className="mdl-grid">
-        <div className="mdl-card mdl-shadow--2dp">
-          <div className="mdl-card__title mdl-color--light-blue-600 mdl-color-text--white">
-            <h2 className="mdl-card__title-text">Vendor upload test</h2>
+    <div className="learn-content mdl-typography--text-center">
+    <div className="logo-font learn-slogan"></div>
+    <a name="top" /> 
+    <div className="learn-content mdl-typography--text-center" style={{width: '80%', margin: 'auto'}}>
+        <div className="grid">
+        <div className="card mdl-shadow--2dp">
+          <div className="card__title mdl-color--light-blue-600 mdl-color-text--white">
+            <h2 className="card__title-text">Vendor upload test</h2>
           </div>
-          <div className="mdl-card__supporting-text mdl-color-text--grey-600" id="messagesDiv">
+          <div className="card__supporting-text mdl-color-text--white-600" id="messagesDiv">
             <h6>Choose File to upload</h6>
-            <input type="file" id="file" name="file"/>
+            <input type="file" id="file" name="file" onChange={this.handleFileSelect}/>
             <h6>File review URL:</h6>
             <span id="linkbox"></span>
           </div>
-        </div>
+            </div>
       </div>
     </div>
   </div>
--- Web/component/Firebase.js
+++ Web/component/Firebase.js
@@ -1,9 +1,11 @@
 import firebase from 'firebase';
 import {FIREBASE_CONFIG} from '../../firebase.config.js';
+import ComponentUpload from './ComponentUpload.js';
 
-export const firebaseApp = firebase.initializeApp(FIREBASE_CONFIG);
+export const firebaseApp = firebase.initializeApp(FIREBASE_CONFIG,'LearnEnergy');
 export const firebaseAuth = firebaseApp.auth();
 export const firebaseDb = firebaseApp.database();
+export const firebaseStorage = firebaseApp.storage().ref();
 
 var FirebaseTools = {
 
@@ -23,6 +25,20 @@ var FirebaseTools = {
 		firebaseAuth.signOut();
 		console.log('User logged out!');
 	},
+    
+    //function fo vendor upload demo
+    vendorUpload: function(fileObj){
+        var url='';
+        firebaseAuth.signInAnonymously();
+        firebaseStorage.child('vendors/'+fileObj.fileName).put(fileObj.file,fileObj.metadata).then(function(snapshot){
+        //use then() to avoid asynchronous case
+        url = snapshot.metadata.downloadURLs[0];
+        document.getElementById('linkbox').innerHTML = '<a href="' + url + '">Click For File</a>';
+        console.log('Upload successfully');
+        }).catch(function(error) {
+        console.error('Upload failed:', error);
+      });;
+    },
 
 }
 
