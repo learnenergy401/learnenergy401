d6a068669678a794701fccd7439b17fd9ba41464
Chris <lisheung@ualberta.ca>
2016-10-15 19:38:01 -0600

    added alerts for login, registering, until we can go to a welcome page.

--- Web/component/Firebase.js
+++ Web/component/Firebase.js
@@ -12,16 +12,20 @@ var FirebaseTools = {
 			email: user.email,
 			password: user.pw
 		});
+		alert("Thank you for registering for LearnEnergy Marketplace." +"\n" + "We will be in contact with you shortly.");
 	},
 
 	loginUser: (user) => {
 		firebaseAuth.signInWithEmailAndPassword(user.email, user.pw);
         console.log('User signed in!');
+				// Until we can link to a homepage that doesn't look the same as when you are not signed in
+				alert("You have signed in:" +"\n" + user.email);
 	},
 
 	logoutUser: () => {
 		firebaseAuth.signOut();
 		console.log('User logged out!');
+		alert("You have logged out. See you later.");
 	},
 
 }
