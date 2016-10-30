97d339e41996cf06f3fa504f0c9eb6b397cf2de0
Chris <lisheung@ualberta.ca>
2016-10-29 21:48:06 -0600

    admin displays emails

--- Web/component/Admin.js
+++ Web/component/Admin.js
@@ -4,6 +4,8 @@ import FirebaseTools from './Firebase.js'
 import React, { Component } from 'react'
 import { Textfield,Grid,Cell,Card,CardText, CardActions, Button } from 'react-mdl';
 import store from './Store.js'
+import LearnHeader from './Header.js'
+import LearnFooter from './Footer.js'
 
 import { connect } from "react-redux"
 import { fetchUsers,getCurrentUser } from "./Actions/userActions"
@@ -18,7 +20,7 @@ class Admin extends Component {
 
     fetchRole() { // pass email to fetchRole for it to see role
         this.props.dispatch(fetchRole(email))
-    }   
+    }
 
     fetchUsers() {
         this.props.dispatch(fetchUsers())
@@ -38,34 +40,53 @@ class Admin extends Component {
     }
 
     render() {
+        var EMAILS = [];
         const {user} = this.props
-        console.log(user)
-        console.log(user.user)
+        //console.log(user)
+        //console.log(user.user)
         var keys
         if (user.user != null) {
             keys = Object.keys(user.user)
-            for (var count=0; count<=keys.length-1; count++) {      
+            for (var count=0; count<=keys.length-1; count++) {
                 var key_name = keys[count]
-                console.log(user.user[key_name].email)
+                EMAILS.push(user.user[key_name].email)
+                EMAILS.push(<br/>)
+                //console.log(user.user[key_name].email)
+                //console.log(EMAILS + "here")
             }
         }
         return(
-        <div>
-            Hello <input type = "text" placeholder = "Admin usename here"/>
-            <h1>TODO</h1>
-            <p><b>display users with check and X next to name </b></p>
-            <p><b>display users with check and X next to name </b></p>
-            <p><b>on check click, user is approved.</b></p>
-            <p><b>on X click, user is refused.</b></p>
-            {/*
-                DISPLAY THE USERS INFORMATION HERE WITH CHECKBOXES 
-            */}
 
-            <Button accent ripple onClick={this.approve} type="submit" className="mdl-color-text--indigo btn btn-primary">Approve</Button>
-            <Button accent ripple onClick={this.reject} type="submit" className="mdl-color-text--indigo btn btn-primary">Reject</Button>
-        
+        <div>
+          <LearnHeader/>
+
+          <div className="learn-content mdl-typography--text-center">
+          <div className="logo-font learn-slogan"></div>
+          <a name="top" />
+          <div className="learn-content mdl-typography--text-center" style={{width: '80%', margin: 'auto'}}>
+            <div className="grid">
+              <div className="card mdl-shadow--2dp">
+                <div className="card__title mdl-color--indigo mdl-color-text--white">
+                  <h4 className="card__title-text">Vendor Candidates</h4>
+                </div>
+                <div className="card__supporting-text mdl-color-text--white-600" id="messagesDiv">
+
+                  <h4> {EMAILS} </h4>
+
+                  <Button accent ripple onClick={this.approve} type="submit" className="mdl-color-text--indigo btn btn-primary">Approve</Button>
+                  <Button accent ripple onClick={this.reject} type="submit" className="mdl-color-text--indigo btn btn-primary">Reject</Button>
+                </div>
+
+                </div>
+            </div>
+          </div>
         </div>
-        
+
+
+
+          <LearnFooter/>
+        </div>
+
         );
     }
 }
