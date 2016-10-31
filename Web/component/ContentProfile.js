4b7d1e0ca6ff30f1e53d13cbc839168cbdc42ca5
joshdeng <j.deng7@hotmail.com>
2016-10-30 19:10:19 -0600

    profile menubar done

--- Web/component/ContentProfile.js
+++ Web/component/ContentProfile.js
@@ -1,33 +1,55 @@
 import React, { Component } from 'react'
 import {Content, Card,CardTitle,List,ListItem,ListItemContent,Layout} from 'react-mdl';
 import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router'
-
+import { connect } from "react-redux"
 
 import "../extra/material.js"
 
 
+@connect((store) => {
+  return {
+    user: store.user,
+    profile: store.profile
+  };
+})/*dont add semicolon here!*/
 
 
 class ContentProfile extends Component {
     render(){
         
+        const {profile} = this.props
         
-        return(
-              <Content className="learn-content">
-                   
-                    <List>
-                      <ListItem>
-                        <ListItemContent icon="person">Bryan Cranston</ListItemContent>
-                      </ListItem>
-                      <ListItem>
-                        <ListItemContent icon="person">Aaron Paul</ListItemContent>
-                      </ListItem>
-                      <ListItem>
-                        <ListItemContent icon="person">Bob Odenkirk</ListItemContent>
-                      </ListItem>
-                    </List>
-                </Content>
-        );
+        if (profile.menu == 0){
+            return(
+                  <Content className="learn-content">
+                        <List>
+                          <ListItem>
+                            <ListItemContent icon="person">gg1</ListItemContent>
+                          </ListItem>
+                        </List>
+                    </Content>
+            );
+        }else if (profile.menu == 1){
+            return(
+                <Content className="learn-content">
+                        <List>
+                          <ListItem>
+                            <ListItemContent icon="person">gg2</ListItemContent>
+                          </ListItem>
+                        </List>
+                    </Content>
+            )
+        }else{
+            return(
+                <Content className="learn-content">
+                        <List>
+                          <ListItem>
+                            <ListItemContent icon="person">gg3</ListItemContent>
+                          </ListItem>
+                        </List>
+                    </Content>
+            )
+        }
     }
 };
 
--- Web/component/Reducers/index.js
+++ Web/component/Reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux"
 
 import user from "./userReducer"
+import profile from "./profileReducer"
 
 export default combineReducers({
-  user,
+    user,
+    profile,
 })
