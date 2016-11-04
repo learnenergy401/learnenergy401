66b53f6c68bca19da96d86d75e71a3e050e97036
jph899 <Jimmy@uws101400usr.uws.ualberta.ca>
2016-11-04 01:14:05 -0600

    JSdoc for firebase.js

--- Web/component/Firebase.js
+++ Web/component/Firebase.js
@@ -2,12 +2,40 @@ import firebase from 'firebase';
 import {FIREBASE_CONFIG} from '../../firebase.config.js';
 import ComponentUpload from './ComponentUpload.js';
 
+/**
+ * Initializes the firebase App 
+ * @returns {object} firebaseApp - Returns instance of firebase.
+ */
 export const firebaseApp = firebase.initializeApp(FIREBASE_CONFIG,'LearnEnergy');
+
+/**
+ * Auth of firebaseApp 
+ * @returns {object} firebaseAuth - Returns auth instance of firebase.
+ */
 export const firebaseAuth = firebaseApp.auth();
+
+/**
+ * Db of firebaseApp
+ * @returns {object} firebaseDb - Returns db instance of firebase.
+ */
 export const firebaseDb = firebaseApp.database();
+
+/**
+ * Storage of firebaseApp 
+ * @returns {object} firebaseStorage - Returns storage instance of firebase.
+ */
 export const firebaseStorage = firebaseApp.storage().ref();
 
+/**
+ * Initializes a second instance of firebase App 
+ * @returns {object} firebaseApp2 - Returns instance of firebase.
+ */
 export const firebaseApp2 = firebase.initializeApp(FIREBASE_CONFIG);
+
+/**
+ * Auth of firebaseApp2
+ * @returns {object} firebaseAuth2 - Returns second auth instance of firebase.
+ */
 export const firebaseAuthInstance = firebaseApp2.auth();
 
 import { connect } from "react-redux"
