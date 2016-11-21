88904a5f3b70882a3404aa64c42c8c02c1d003cb
joshdeng <j.deng7@hotmail.com>
2016-11-20 18:54:47 -0700

    edit tags done

--- Web/component/Profile/ContentCourseUpdate.js
+++ Web/component/Profile/ContentCourseUpdate.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Content, Card,CardTitle,CardText,Layout,Textfield,CardActions,Button} from 'react-mdl';
+import {Content, Card,CardTitle,CardText,Layout,Textfield,Chip,CardActions,Button} from 'react-mdl';
 import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router'
 import { connect } from "react-redux"
 
@@ -8,6 +8,7 @@ import { updateCourse } from "../Actions/courseActions"
 import { uploadCourse } from "../Actions/courseActions"
 import { fetchVendorCourse } from "../Actions/courseActions"
 import { fetchACourse} from "../Actions/courseActions"
+import { saveTags,addTag,deleteTag} from "../Actions/tagActions"
 import Toast from "../Toast.js"
 
 var componentStyle = {
@@ -16,18 +17,44 @@ var componentStyle = {
 var formStyle = {
     marginTop: '5%'
 }
-
+var count = 1
 
 @connect((store) => {
   return {
     user: store.user,
     course: store.course,
-    profile: store.profile
+    profile: store.profile,
+    tags: store.tags
   };
 })/*dont add semicolon here!*/
 
 
+
 class ContentCourseUpdate extends Component {
+    contains(a, text) {
+        var i = a.length;
+        while (i--) {
+           if (a[i].text == text) {
+               return true;
+           }
+        }
+        return false;
+    }
+    handleAddTag(){
+      const tags = this.props.tags
+        var tagText = document.getElementById("addTagUpd").value
+        if (tagText){
+          if(!this.contains(tags,tagText)){
+            if(tags.length<5){
+              this.props.dispatch(addTag(tagText));
+            }
+          }
+        }
+        
+    }
+    handleDelete(text){
+        this.props.dispatch(deleteTag(text))
+    }
     componentWillMount(){
         const {user} = this.props
         const {course}=this.props
@@ -39,6 +66,12 @@ class ContentCourseUpdate extends Component {
         document.getElementById("courseName").value=course.aCourse.courseName;
         document.getElementById("courseDescription").value=course.aCourse.courseDescription;
         document.getElementById("courseVideoId").value=course.aCourse.courseVideoId;
+        console.log(course.aCourse.courseTags)
+        if (course.aCourse.courseTags&&count==1){
+            this.props.dispatch(saveTags(course.aCourse.courseTags));
+            count--
+        }
+
     }
     
     updateCourse(){
@@ -51,7 +84,9 @@ class ContentCourseUpdate extends Component {
         var courseVideoId = document.getElementById("courseVideoId").value;
         var courseVendorEmail = user.email
         var courseID=prevID;
-        var newCourse = {courseName, courseDescription, courseVendorEmail,courseVideoId,courseID}
+        const tags = this.props.tags
+        var courseTags = tags
+        var newCourse = {courseName, courseDescription, courseVendorEmail,courseVideoId,courseID,courseTags}
         this.props.dispatch(updateCourse(newCourse,courseID));
         
     }
@@ -60,20 +95,32 @@ class ContentCourseUpdate extends Component {
     * @return {html} - returns course profile depending on type of user
     */
     render(){
+        const tags = this.props.tags
+        const mappedTags = tags.map(tag => 
+            <Chip style={{marginRight:"3px"}} key={tag.text} onClose={()=>(this.handleDelete(tag.text))}>{tag.text}</Chip>
+        )
         return(
             <Content className="learn-content">
                 <div className="android-content mdl-layout__content">
                         <a name="top" />
                         <div style={{width: '80%', margin: 'auto'}}>
                             <CardText style={componentStyle}>
-                                <Textfield floatingLabel label="courseName" className="form-control" ref="courseName" id="courseName"/>
+                                <Textfield floatingLabel label="courseName" className="form-control"  id="courseName"/>
                                 </CardText>
                             <CardText style={componentStyle}>
-                                <Textfield floatingLabel label="courseDescription" ref="pw" type="courseDescription" className="form-control" id="courseDescription"/>
+                                <Textfield floatingLabel label="courseDescription" type="courseDescription" className="form-control" id="courseDescription"/>
                             </CardText>
                             <CardText style={componentStyle}>
-                                <Textfield floatingLabel label="courseVideoId" ref="pw" type="courseVideoId" className="form-control" id="courseVideoId"/>
+                                <Textfield floatingLabel label="courseVideoId"  type="courseVideoId" className="form-control" id="courseVideoId"/>
                             </CardText>
+                            <CardText style={componentStyle}>
+                                <Textfield style={{width:"100px"}} floatingLabel maxLength="20" label="Tags"  type="addTag" className="form-control" id="addTagUpd"/>
+                                <Button onClick = {this.handleAddTag.bind(this)}>add tag</Button>
+                                
+                            </CardText>
+
+                            <CardText>{mappedTags}</CardText>
+                        
                             <CardActions style={componentStyle}>
                                 <Button onClick={this.updateCourse.bind(this)} accent ripple  className="mdl-color-text--indigo btn btn-primary">Update Detail</Button>
                             </CardActions>
