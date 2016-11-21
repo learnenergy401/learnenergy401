db518b9793c58196d53a2163828617ec3e82b14d
joshdeng <j.deng7@hotmail.com>
2016-11-20 19:00:32 -0700

    sb

--- Web/component/Reducers/index.js
+++ Web/component/Reducers/index.js
@@ -3,11 +3,9 @@ import { combineReducers } from "redux"
 import user from "./userReducer"
 import profile from "./profileReducer"
 import course from "./courseReducer"
-import tags from "./tagReducer"
 
 export default combineReducers({
     user,
     profile,
     course,
-    tags,
 })
