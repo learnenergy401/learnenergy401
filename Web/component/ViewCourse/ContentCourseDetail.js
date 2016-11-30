55ee1f1fbf86c1911a68788b1c1d80725dd9417b
joshdeng <j.deng7@hotmail.com>
2016-11-30 05:42:35 -0700

    course detail revamped

--- Web/component/ViewCourse/ContentCourseDetail.js
+++ Web/component/ViewCourse/ContentCourseDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Content,CardText,CardActions,Card,CardTitle, Button,Navigation} from 'react-mdl';
+import {Content,CardText,CardActions,Card,CardTitle, Button,Navigation,Grid,Cell} from 'react-mdl';
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 import { connect } from "react-redux"
 import { fetchACourse, addCoursePurchaser } from "../Actions/courseActions"
@@ -122,8 +122,8 @@ class ContentCourseDetail extends Component {
     	const {course}=this.props
         const {user} = this.props
         const opts = {
-            height: '480',
-            width: '640',
+            height: '320',
+            width: '570',
             playerVars: { // https://developers.google.com/youtube/player_parameters
                 autoplay: 0,
                 controls: 1,
@@ -138,52 +138,63 @@ class ContentCourseDetail extends Component {
         var buttons1 = []
         var buttons2 = []
         if (user.role == 0) {
-          buttons1.push(<Button accent ripple className="mdl-color--indigo mdl-color-text--white btn btn-primary mdl-shadow--2dp" style={{width:'40%'}} onClick={this.req_EOI.bind(this)}>Request EOI</Button>)
-          buttons2.push(<Button accent ripple className="mdl-color--indigo mdl-color-text--white btn btn-primary mdl-shadow--2dp" style={{width:'40%'}} onClick={this.purchase_course.bind(this)}>Check out</Button> )
+          buttons1.push(<Button accent ripple className="mdl-color--indigo mdl-color-text--white btn btn-primary mdl-shadow--2dp" style={{width:'150px'}} onClick={this.req_EOI.bind(this)}>Request EOI</Button>)
+          buttons2.push(<Button accent ripple className="mdl-color--indigo mdl-color-text--white btn btn-primary mdl-shadow--2dp" style={{width:'150px'}} onClick={this.purchase_course.bind(this)}>Buy Now</Button> )
         }
 
 
         return(
-          <div style={{height:'660px'}}>
+          <div>
+            <div style= {{height:"400px",background:"url(../../images/course.jpg)",padding:"40px",paddingBottom:"0px" }} >
+              <div style= {{height:"400px",width:"55%", background:"white",bottom:"0px" }}>
+                <div style= {{padding:"80px",paddingBottom:"0px" }}>
+                  <div style={{color:"#08287D"}} className="mdl-typography--display-2 ">
+                    {course.aCourse.courseName}
+                  </div>
+                  <p style= {{marginTop:"20px" }} className="mdl-typography--headline mdl-typography--font-thin">
+                    {details}
+                  </p>
+                  <Grid style={{padding:"0px",marginTop:"40px"}} >
+                    <Cell style={{margin:"0px"}} col={7}>
+                      <p className="mdl-typography--font-regular">
+                        Want custom training?
+                      </p>
+                        {buttons1}
+                    </Cell>
+                    <Cell style={{margin:"0px"}} col={5}>
+                      <p className="mdl-typography--font-regular">
+                          Purchase this course?
+                      </p>
+                      {buttons2}
+                    </Cell>
+                  </Grid>
+                </div>
+              </div>
+            </div>
 
-          	  <Card  style={cardStyle} >
-          	  <CardTitle className="mdl-color--indigo mdl-color-text--white mdl-shadow--2dp">{course.aCourse.courseName}</CardTitle>
-          	  <div className="mdl-layout__content">
-              <a name="top" />
-                    <div style={{width: '90%',marginLeft:'80px'}}>
-            {/*<CardText style={componentStyle}>{course.aCourse.courseDescription}</CardText>*/}
-                        <Navigation style={{width:'100%'}}>
-                        <div style={{height:'360px',width:'720px',marginTop:'24px',marginLeft:'0px!important'}}>
-                            <YouTube 
+            <div style= {{height:"360px",background:"#F8F8F8",margin:"40px",marginTop:"0px" }}>
+              <Grid style={{padding:"0px",paddingTop:"20px",margin:"20px",marginLeft:"20px",marginTop:"0px"}}>
+                  <Cell style={{margin:"0px", background:"#F8F8F8",height:"320px"}} col={7}>
+                      <YouTube 
                                 videoId={course.aCourse.courseVideoId}
                                 opts={opts}
                                 onReady={this._onReady}
                             />
+                    </Cell>
+                    <Cell style={{margin:"0px",marginLeft:"70px",padding:"20px",paddingTop:"40px", background:"#F3F3F3"}} col={4}>
+                      <div>
+                      <center>
+                        <div className="mdl-typography--headline">
+                          About The Vendor
                         </div>
-                        <div style={{marginLeft:'12px',marginTop:'24px',marginRight:'12px',width:'40%'}}>
-                            <Card style={{width:'100%'}}>
-                                
-                                <CardTitle className="mdl-color--white mdl-color-text--black mdl-shadow--2dp">Course Description:</CardTitle>
-                                <div style={{marginLeft:'12px',marginTop:'12px'}}><h7>{details}</h7></div>
-                                
-                                <CardTitle className="mdl-color--white mdl-color-text--black mdl-shadow--2dp" style={{marginTop:'24px'}}>Want custom training?</CardTitle>
-                                <div style={{marginLeft:'12px',marginTop:'24px'}}><center>{buttons1}</center></div>
-            
-                                <CardTitle className="mdl-color--white mdl-color-text--black mdl-shadow--2dp" style={{marginTop:'24px'}}>Purchase this course:</CardTitle>
-                                <div style={{marginLeft:'12px',marginTop:'24px'}}><center>{buttons2}</center></div>
-            
-                            </Card>
-                            <div>
-                                
-                            </div>
-                        </div>
-                        
-                        </Navigation>
-                    </div>
-                               
-                </div>
-               </Card>
-
+                        <p className="mdl-typography--headline mdl-typography--font-thin ">
+                            {course.aCourse.courseVendorEmail}
+                        </p>
+                      </center>
+                      </div>
+                    </Cell>
+              </Grid>
+            </div>
           </div>
         );
     }
